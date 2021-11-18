9683ebee43c0a65127428d77abe2a6cb98a8ee3a
Eric Lundby <Eric.Lundby@gmail.com>
2021-11-18 10:54:47 -0700

    Add import

--- src/android/QRScanner.java
+++ src/android/QRScanner.java
@@ -32,6 +32,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
+import android.util.DisplayMetrics;
 
 
 @SuppressWarnings("deprecation")
